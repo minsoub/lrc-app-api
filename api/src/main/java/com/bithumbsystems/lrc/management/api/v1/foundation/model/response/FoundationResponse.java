33c55fd0699046d4b1a8218d1de7a875ae473a6b
colorart <colorart@bithumbsystems.com>
2022-06-19 11:39:00 +0900

    재단, 재단정보 API 추가

--- api/src/main/java/com/bithumbsystems/lrc/management/api/v1/foundation/model/response/FoundationResponse.java
+++
@@ -1,5 +0,0 @@
-package com.bithumbsystems.lrc.management.api.v1.foundation.model.response;
-
-public class FoundationResponse {
-
-}
--- api/src/main/java/com/bithumbsystems/lrc/management/api/v1/foundation/service/FoundationService.java
+++
@@ -1,8 +0,0 @@
-package com.bithumbsystems.lrc.management.api.v1.foundation.service;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class FoundationService {
-
-}
