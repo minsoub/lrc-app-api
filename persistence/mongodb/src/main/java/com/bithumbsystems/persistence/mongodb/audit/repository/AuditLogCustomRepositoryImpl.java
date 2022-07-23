5def1bd9a08e1b5534bbd61a97108f2e72c87032
jms <minsoub@bithumbsystems.com>
2022-07-23 18:16:06 +0900

    서비스로그 조회 검색 조건 수정

--- persistence/mongodb/src/main/java/com/bithumbsystems/persistence/mongodb/audit/repository/AuditLogCustomRepositoryImpl.java
+++ persistence/mongodb/src/main/java/com/bithumbsystems/persistence/mongodb/audit/repository/AuditLogCustomRepositoryImpl.java
@@ -25,10 +25,7 @@ public class AuditLogCustomRepositoryImpl implements AuditLogCustomRepository {
 
         // TODO: my_site_id 고려
         query.addCriteria(Criteria.where("create_date").gte(fromDate.atTime(0, 0, 0)).lte(toDate.atTime(23, 59, 59)));
-                //.addCriteria(Criteria.where("my_site_id").is(mySiteId));
-        query.addCriteria(new Criteria().andOperator(
-                Criteria.where("my_site_id").is(mySiteId)
-        ));
+        query.addCriteria(Criteria.where("my_site_id").is(mySiteId));
         if (StringUtils.isNotEmpty(keyword)) {   //서비스 로그 관리 목록
             query.addCriteria(new Criteria().orOperator(
                     Criteria.where("email").regex(".*" + keyword + ".*", "i"),
