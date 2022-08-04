22381cb17ac7da3c54da961712c2ae96b7b16b88
rihongo-at-807380035085 <3zaHkd3zJ87T1icEEOSTNLAHlAdGniO9sZT2LJlmeo4=>
2022-08-04 10:17:15 +0900

    camel case 수정

--- api/src/main/java/com/bithumbsystems/lrc/management/api/core/config/resolver/CustomArgumentResolver.java
+++ api/src/main/java/com/bithumbsystems/lrc/management/api/core/config/resolver/CustomArgumentResolver.java
@@ -34,7 +34,7 @@ public class CustomArgumentResolver implements HandlerMethodArgumentResolver {
     final String AUTHORIZATION = "Authorization";
     final String BEARER_TYPE = "Bearer";
     final String MY_SITE_ID = "my_site_id";
-      final String USER_IP = "user_ip";
+    final String USER_IP = "user_ip";
 
     for (String value : Objects.requireNonNull(
         exchange.getRequest().getHeaders().get(AUTHORIZATION))) {
@@ -43,22 +43,24 @@ public class CustomArgumentResolver implements HandlerMethodArgumentResolver {
       }
     }
 
-      String my_site_id = exchange.getRequest().getHeaders().get(MY_SITE_ID) != null ?
-              Objects.requireNonNull(exchange.getRequest().getHeaders().get(MY_SITE_ID)).get(0) : "";
-      String user_ip = exchange.getRequest().getHeaders().get(USER_IP) != null ?
-              Objects.requireNonNull(exchange.getRequest().getHeaders().get(USER_IP)).get(0) : "";
+    String mySiteId = exchange.getRequest().getHeaders().get(MY_SITE_ID) != null ?
+        Objects.requireNonNull(exchange.getRequest().getHeaders().get(MY_SITE_ID)).get(0) : "";
+    String userIp = exchange.getRequest().getHeaders().get(USER_IP) != null ?
+        Objects.requireNonNull(exchange.getRequest().getHeaders().get(USER_IP)).get(0) : "";
     log.debug(token);
     return reactiveJwtDecoder.decode(token)
-        .flatMap((jwt) -> {
-          if(StringUtils.isAnyEmpty(jwt.getClaimAsString("account_id")) || jwt.getClaim("ROLE") == null) {
+        .flatMap(jwt -> {
+          if (StringUtils.isAnyEmpty(jwt.getClaimAsString("account_id"))
+              || jwt.getClaim("ROLE") == null) {
             return Mono.error(new InvalidTokenException(ErrorCode.INVALID_TOKEN));
           }
           log.debug("Role => {}", jwt.getClaimAsString("ROLE"));
           final var accountId = jwt.getClaimAsString("account_id");
-          final var roles = (JSONArray)jwt.getClaim("ROLE");
+          final var roles = (JSONArray) jwt.getClaim("ROLE");
           final var email = jwt.getClaimAsString("user_id");
-          final var mySiteId = my_site_id; // jwt.getSubject();
-          return Mono.just(new Account(accountId, roles.stream().map(Object::toString).collect(Collectors.toSet()), email, user_ip, mySiteId));
+          return Mono.just(new Account(accountId,
+              roles.stream().map(Object::toString).collect(Collectors.toSet()), email, userIp,
+              mySiteId));
         });
   }
 }
