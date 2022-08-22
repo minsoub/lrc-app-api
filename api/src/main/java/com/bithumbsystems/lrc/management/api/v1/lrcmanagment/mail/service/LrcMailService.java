69b4113e282a1eafe370d8dbd0b3dbbd74f8f00c
hgjeong <hgjeong@bithumbsystems.com>
2022-08-23 01:19:05 +0900

    채팅 알림 메일 프로젝트 참여자 전원에게 발송 추가

--- api/src/main/java/com/bithumbsystems/lrc/management/api/v1/lrcmanagment/mail/service/LrcMailService.java
+++ api/src/main/java/com/bithumbsystems/lrc/management/api/v1/lrcmanagment/mail/service/LrcMailService.java
@@ -1,9 +1,14 @@
 package com.bithumbsystems.lrc.management.api.v1.lrcmanagment.mail.service;
 
+import com.bithumbsystems.lrc.management.api.core.config.properties.AwsProperties;
+import com.bithumbsystems.lrc.management.api.core.util.AES256Util;
 import com.bithumbsystems.lrc.management.api.core.util.message.MessageService;
+import com.bithumbsystems.persistence.mongodb.lrcmanagment.project.useraccount.service.UserAccountDomainService;
+import com.bithumbsystems.persistence.mongodb.lrcmanagment.project.useraccount.service.UserInfoDomainService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 import reactor.core.publisher.Mono;
 
 @Service
@@ -11,8 +16,23 @@ import reactor.core.publisher.Mono;
 @Slf4j
 public class LrcMailService {
     private final MessageService messageService;
+    private final UserAccountDomainService userAccountDomainService;
+    private final UserInfoDomainService userInfoDomainService;
+    private final AwsProperties awsProperties;
     public Mono<Boolean> sendEmail(String email, String type) {
         messageService.sendMail(email, type);
         return Mono.just(true);
     }
+
+    public Mono<Boolean> sendEmailToProjectUser(String projectId, String type) {
+        return userAccountDomainService.findByProjectId(projectId).flatMap(projectUser -> {
+            return userInfoDomainService.findById(projectUser.getUserAccountId()).flatMap(userAccount -> {
+                String email = (StringUtils.hasLength(projectUser.getContactEmail()))? AES256Util.decryptAES(awsProperties.getKmsKey(), projectUser.getContactEmail()) : AES256Util.decryptAES(awsProperties.getKmsKey(), userAccount.getEmail());
+                log.debug("sendEmailToProjectUser:{}:{}", email, type);
+                messageService.sendMail(email, type);
+                return Mono.just(true);
+            });
+        }).then(Mono.just(true));
+    }
+
 }
