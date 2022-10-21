eab5abed1658fee25dedfa51d7de63c4fdfdefe6
hgjeong <hgjeong@bithumbsystems.com>
2022-10-21 11:21:45 +0900

    커뮤니케이션 알림 메일 수정

--- api/src/main/java/com/bithumbsystems/lrc/management/api/core/model/enums/MailForm.java
+++ api/src/main/java/com/bithumbsystems/lrc/management/api/core/model/enums/MailForm.java
@@ -8,7 +8,7 @@ import lombok.RequiredArgsConstructor;
 public enum MailForm {
   DEFAULT("subject", "mail/default.html"),
   KOR("[Bithumb 거래지원 및 문의] 커뮤니케이션 알림", "mail/mailform_kor.html"),
-  EN("[Bithumb Market support] Communication Notifications", "mail/mailform_en.html");
+  EN("[Bithumb Listing] Communication Notifications", "mail/mailform_en.html");
 
 
   private final String subject;
