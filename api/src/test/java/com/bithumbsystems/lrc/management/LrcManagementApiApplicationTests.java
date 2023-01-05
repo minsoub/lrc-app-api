acd3ed74daf0f36d48926c556b3dd4f246ff0fe3
leeturn <leeturn@bithumbsystems.com>
2023-01-05 15:59:12 +0900

    Refactor: 미사용 test code 주석 처리 및 jira 연동 테스트
    Resolves: @LIST-227

--- api/src/test/java/com/bithumbsystems/lrc/management/LrcManagementApiApplicationTests.java
+++ api/src/test/java/com/bithumbsystems/lrc/management/LrcManagementApiApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class LrcManagementApiApplicationTests {
 
-  @Test
-  void contextLoads() {
-  }
+  //@Test
+  //void contextLoads() {
+  //}
 
 }
