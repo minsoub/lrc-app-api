d0ca8932edceafb5d24acd859307c69b215654c0
leeturn <leeturn@bithumbsystems.com>
2023-01-09 23:07:05 +0900

    Refactor: Google Code Convention 적용, JavaDoc 추가, Source Refactoring
    이슈 연관 파일에 대해
     - Google Code Convention 적용
     - SonaLint, CheckStyle 기반 Code Refactoring
     - 누락된 JavaDoc 추가, Convention 맞게 수정
    
    Resolves: @LIST-279

--- api/src/main/java/com/bithumbsystems/lrc/management/api/v1/lrcmanagment/project/projectinfo/controller/ProjectInfoController.java
+++ api/src/main/java/com/bithumbsystems/lrc/management/api/v1/lrcmanagment/project/projectinfo/controller/ProjectInfoController.java
@@ -4,66 +4,80 @@ import com.bithumbsystems.lrc.management.api.core.config.resolver.Account;
 import com.bithumbsystems.lrc.management.api.core.config.resolver.CurrentUser;
 import com.bithumbsystems.lrc.management.api.core.model.response.SingleResponse;
 import com.bithumbsystems.lrc.management.api.v1.lrcmanagment.project.projectinfo.model.request.ProjectInfoRequest;
+import com.bithumbsystems.lrc.management.api.v1.lrcmanagment.project.projectinfo.model.response.ProjectInfoResponse;
 import com.bithumbsystems.lrc.management.api.v1.lrcmanagment.project.projectinfo.service.ProjectInfoService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
-import io.swagger.v3.oas.annotations.enums.ParameterIn;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 
+/**
+ * The type Project info controller.
+ */
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("lrc/lrcmanagment/project")
 @Tag(name = "프로젝트 정보", description = "프로젝트 정보 API")
 public class ProjectInfoController {
 
-    private final ProjectInfoService projectInfoService;
+  private final ProjectInfoService projectInfoService;
 
-    /**
-     * 프로젝트 정보 1개 id 찾기
-     * @param projectId
-     * @return ProjectInfoResponse Object
-     */
-    @GetMapping("/project-info/{projectId}")
-    @Operation(summary = "거래지원 관리 - 프로젝트 정보 1개 id 찾기", description = "projectId를 이용하여 프로젝트 정보를 조회합니다.", tags = "사이트 운영 > 거래지원 관리 > 프로젝트 관리 > 프로젝트 정보 > 1개 찾기")
-    public ResponseEntity<Mono<?>> getProjectInfo(@Parameter(name = "projectId", description = "project 의 projectId", in = ParameterIn.PATH)
-                                                      @PathVariable("projectId") String projectId) {
-        return ResponseEntity.ok().body(projectInfoService.findByProjectId(projectId)
-                .map(c -> new SingleResponse(c))
-        );
-    }
+  /**
+   * 프로젝트 정보 1개 id 찾기.
+   *
+   * @param projectId the project id
+   * @return ProjectInfoResponse Object
+   */
+  @GetMapping("/project-info/{projectId}")
+  @Operation(summary = "거래지원 관리 - 프로젝트 정보 1개 id 찾기", description = "projectId를 이용하여 프로젝트 정보를 조회합니다.",
+      tags = "사이트 운영 > 거래지원 관리 > 프로젝트 관리 > 프로젝트 정보 > 1개 찾기")
+  public ResponseEntity<Mono<SingleResponse<ProjectInfoResponse>>> getProjectInfo(
+      @Parameter(description = "project 의 projectId") @PathVariable("projectId") String projectId) {
+    return ResponseEntity.ok().body(projectInfoService.findByProjectId(projectId)
+        .map(SingleResponse::new)
+    );
+  }
 
-    /**
-     * 프로젝트 정보 1개 저장
-     * @param projectInfoRequest
-     * @return ProjectInfoResponse Object
-     */
-    @PostMapping("/project-info")
-    @Operation(summary = "거래지원 관리 - 프로젝트 정보 1개 저장", description = "프로젝트 정보를 저장합니다.", tags = "사이트 운영 > 거래지원 관리 > 프로젝트 관리 > 프로젝트 정보 > 1개 저장")
-    public ResponseEntity<Mono<?>> createProjectInfo(@Parameter(name = "project Object", description = "프로젝트 의 모든 정보", in = ParameterIn.PATH)
-                                                         @RequestBody ProjectInfoRequest projectInfoRequest) {
-        return ResponseEntity.ok().body(projectInfoService.create(projectInfoRequest)
-                .map(c -> new SingleResponse(c))
-        );
-    }
+  /**
+   * 프로젝트 정보 1개 저장.
+   *
+   * @param projectInfoRequest the project info request
+   * @return ProjectInfoResponse Object
+   */
+  @PostMapping("/project-info")
+  @Operation(summary = "거래지원 관리 - 프로젝트 정보 1개 저장", description = "프로젝트 정보를 저장합니다.", tags = "사이트 운영 > 거래지원 관리 > 프로젝트 관리 > 프로젝트 정보 > 1개 저장")
+  public ResponseEntity<Mono<SingleResponse<ProjectInfoResponse>>> createProjectInfo(
+      @Parameter(name = "project Object", description = "프로젝트 의 모든 정보") @RequestBody ProjectInfoRequest projectInfoRequest) {
+    return ResponseEntity.ok().body(projectInfoService.create(projectInfoRequest)
+        .map(SingleResponse::new)
+    );
+  }
 
-    /**
-     * 프로젝트 정보 업데이트
-     * @param projectId
-     * @param projectInfoRequest
-     * @return ProjectInfoResponse Object
-     */
-    @PutMapping("/project-info/{projectId}")
-    @Operation(summary = "거래지원 관리 - 프로젝트 정보 업데이트", description = "프로젝트 정보를 수정하여 저장합니다.", tags = "사이트 운영 > 거래지원 관리 > 프로젝트 관리 > 프로젝트 정보 > 1개 수정")
-    public ResponseEntity<Mono<?>> updateProjectInfo(@Parameter(name = "projectId", description = "project 의 projectId", in = ParameterIn.PATH)
-                                                         @PathVariable("projectId") String projectId,
-                                                     @RequestBody ProjectInfoRequest projectInfoRequest,
-                                                     @Parameter(hidden = true) @CurrentUser Account account) {
-        return ResponseEntity.ok().body(projectInfoService.updateProjectInfo(projectId, projectInfoRequest, account)
-                .map(c -> new SingleResponse(c))
-        );
-    }
+  /**
+   * 프로젝트 정보 업데이트.
+   *
+   * @param projectId          the project id
+   * @param projectInfoRequest the project info request
+   * @param account            the account
+   * @return ProjectInfoResponse Object
+   */
+  @PutMapping("/project-info/{projectId}")
+  @Operation(summary = "거래지원 관리 - 프로젝트 정보 업데이트", description = "프로젝트 정보를 수정하여 저장합니다.", tags = "사이트 운영 > 거래지원 관리 > 프로젝트 관리 > 프로젝트 정보 > 1개 수정")
+  public ResponseEntity<Mono<SingleResponse<ProjectInfoResponse>>> updateProjectInfo(
+      @Parameter(description = "project 의 projectId") @PathVariable("projectId") String projectId,
+      @RequestBody ProjectInfoRequest projectInfoRequest,
+      @Parameter(hidden = true) @CurrentUser Account account) {
+    return ResponseEntity.ok().body(projectInfoService.updateProjectInfo(projectId, projectInfoRequest, account)
+        .map(SingleResponse::new)
+    );
+  }
 }
--- api/src/main/java/com/bithumbsystems/lrc/management/api/v1/lrcmanagment/project/projectinfo/model/response/ProjectInfoResponse.java
+++ api/src/main/java/com/bithumbsystems/lrc/management/api/v1/lrcmanagment/project/projectinfo/model/response/ProjectInfoResponse.java
@@ -1,55 +1,56 @@
 package com.bithumbsystems.lrc.management.api.v1.lrcmanagment.project.projectinfo.model.response;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import java.time.LocalDate;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-
+/**
+ * The type Project info response.
+ */
 @Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
 @Schema(description = "프로젝트 정보")
 public class ProjectInfoResponse {
-    
-    private String id;
 
-    @Schema(description = "프로젝트 코드")
-    private String projectId;               //프로젝트 코드
+  private String id;
 
-    @Schema(description = "사업계열")
-    private String businessCode;            //사업계열
+  @Schema(description = "프로젝트 코드")
+  private String projectId;
 
-    @Schema(description = "사업계열명")
-    private String businessName;
+  @Schema(description = "사업계열")
+  private String businessCode;
 
-    @Schema(description = "네트워크")
-    private String networkCode;             //네트워크계열
+  @Schema(description = "사업계열명")
+  private String businessName;
 
-    @Schema(description = "네트워크명")
-    private String networkName;
+  @Schema(description = "네트워크")
+  private String networkCode;
 
-    @Schema(description = "백서링크")
-    private String whitepaperLink;          //백서링크
+  @Schema(description = "네트워크명")
+  private String networkName;
 
-    @Schema(description = "컨트렉트 주소")
-    private String contractAddress;         //컨트렉트 주소
+  @Schema(description = "백서링크")
+  private String whitepaperLink;
 
-    @Schema(description = "최초발행일")
-    private LocalDate createDate;       //최초 발행일
+  @Schema(description = "컨트랙트 주소")
+  private String contractAddress;
+
+  @Schema(description = "최초발행일")
+  private LocalDate createDate;
 
   @Schema(description = "상위 사업계열")
-  private String parentBusinessCode;            //사업계열
+  private String parentBusinessCode;
 
   @Schema(description = "상위 사업계열명")
   private String parentBusinessName;
 
   @Schema(description = "상위 네트워크")
-  private String parentNetworkCode;             //네트워크계열
+  private String parentNetworkCode;
 
   @Schema(description = "상위 네트워크명")
   private String parentNetworkName;
