3b53717258da542a9f7c9e9b354252875e101101
leeturn <leeturn@bithumbsystems.com>
2023-01-09 17:53:47 +0900

    Refactor: Google Code Convention 적용, JavaDoc 추가, Source Refactoring
    이슈 연관 파일에 대해
     - Google Code Convention 적용
     - SonaLint, CheckStyle 기반 Code Refactoring
     - 누락된 JavaDoc 추가, Convention 맞게 수정
    
    Resolves: @LIST-222

--- api/src/main/java/com/bithumbsystems/lrc/management/api/v1/dashboard/service/DashBoardService.java
+++ api/src/main/java/com/bithumbsystems/lrc/management/api/v1/dashboard/service/DashBoardService.java
@@ -1,6 +1,5 @@
 package com.bithumbsystems.lrc.management.api.v1.dashboard.service;
 
-
 import com.bithumbsystems.lrc.management.api.v1.dashboard.model.response.DashBoardLineMngResponse;
 import com.bithumbsystems.lrc.management.api.v1.dashboard.model.response.DashBoardStatus;
 import com.bithumbsystems.lrc.management.api.v1.dashboard.model.response.DashBoardStatusCodeResponse;
@@ -17,86 +16,93 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
 
+/**
+ * The type Dash board service.
+ */
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class DashBoardService {
 
-    private final StatusCodeDomainService statusCodeDomainService;
-    private final LineMngDomainService lineMngDomainService;
-    private final FoundationInfoDomainService foundationInfoDomainService;
-    private final ProjectInfoDomainService projectInfoDomainService;
+  private final StatusCodeDomainService statusCodeDomainService;
+  private final LineMngDomainService lineMngDomainService;
+  private final FoundationInfoDomainService foundationInfoDomainService;
+  private final ProjectInfoDomainService projectInfoDomainService;
 
-    /**
-     * 상태값 관리 모두 가져오기
-     * @return DashBoardStatusCodeResponse
-     */
-    public Mono<List<DashBoardStatusCodeResponse>> getStatusCode(LocalDate searchFromDate, LocalDate searchToDate) {
-        return statusCodeDomainService.findAllStatus()
-                .map(statusCode ->
-                        DashBoardStatus.builder()
-                            .id(statusCode.getId())
-                            .name(statusCode.getName())
-                            .parentCode(statusCode.getParentCode())
-                            .orderNo(statusCode.getOrderNo())
-                            .useYn(statusCode.getUseYn())
-                            .build()
-                )
-                .filter(DashBoardStatus::getUseYn)
-                .flatMap(c ->
-                        Mono.just(c)
-                                .zipWith(foundationInfoDomainService.findByCustomContractProcess(c.getId(), searchFromDate, searchToDate).count())
-                                .map(t -> {
-                                    t.getT1().setCount(t.getT2());
-                                    return t.getT1();
-                                })
-                )
-                .collectSortedList(Comparator.comparing(DashBoardStatus::getOrderNo))
-                .map(c -> {
+  /**
+   * 상태값 관리 모두 가져오기.
+   *
+   * @param searchFromDate the search from date
+   * @param searchToDate   the search to date
+   * @return DashBoardStatusCodeResponse status code
+   */
+  public Mono<List<DashBoardStatusCodeResponse>> getStatusCode(LocalDate searchFromDate, LocalDate searchToDate) {
+    return statusCodeDomainService.findAllStatus()
+        .map(statusCode ->
+            DashBoardStatus.builder()
+                .id(statusCode.getId())
+                .name(statusCode.getName())
+                .parentCode(statusCode.getParentCode())
+                .orderNo(statusCode.getOrderNo())
+                .useYn(statusCode.getUseYn())
+                .build()
+        )
+        .filter(DashBoardStatus::getUseYn)
+        .flatMap(c ->
+            Mono.just(c)
+                .zipWith(foundationInfoDomainService.findByCustomContractProcess(c.getId(), searchFromDate, searchToDate).count())
+                .map(t -> {
+                  t.getT1().setCount(t.getT2());
+                  return t.getT1();
+                })
+        )
+        .collectSortedList(Comparator.comparing(DashBoardStatus::getOrderNo))
+        .map(c -> {
 
-                    List<DashBoardStatus> d = c.stream().filter(f -> f.getParentCode() == null || "".equals(f.getParentCode())).collect(Collectors.toList());
+          List<DashBoardStatus> d = c.stream().filter(f -> f.getParentCode() == null || "".equals(f.getParentCode())).collect(Collectors.toList());
 
-                    return d.stream().map(s ->
-                            DashBoardStatusCodeResponse.builder()
-                                    .id(s.getId())
-                                    .name(s.getName())
-                                    .parentCode(s.getParentCode())
-                                    .orderNo(s.getOrderNo())
-                                    .useYn(s.getUseYn())
-                                    .count(s.getCount())
-                                    .children(
-                                            c.stream().filter(f -> f.getParentCode().equals(s.getId()))
-                                                    .sorted(Comparator.comparing(DashBoardStatus::getOrderNo))
-                                                    .collect(Collectors.toList())
-                                    )
-                                    .build()
-                    ).collect(Collectors.toList());
-                });
-    }
+          return d.stream().map(s ->
+              DashBoardStatusCodeResponse.builder()
+                  .id(s.getId())
+                  .name(s.getName())
+                  .parentCode(s.getParentCode())
+                  .orderNo(s.getOrderNo())
+                  .useYn(s.getUseYn())
+                  .count(s.getCount())
+                  .children(
+                      c.stream().filter(f -> f.getParentCode().equals(s.getId()))
+                          .sorted(Comparator.comparing(DashBoardStatus::getOrderNo))
+                          .collect(Collectors.toList())
+                  )
+                  .build()
+          ).collect(Collectors.toList());
+        });
+  }
 
-    /**
-     * 계열관리 통계 모두 가져오기
-     * @return DashBoardLineMngResponse
-     */
-    public Mono<List<DashBoardLineMngResponse>> getLineMng() {
-        return lineMngDomainService.findAll()
-                .map(lineMng ->
-                        DashBoardLineMngResponse.builder()
-                                .id(lineMng.getId())
-                                .name(lineMng.getName())
-                                .type(lineMng.getType())
-                                .useYn(lineMng.getUseYn())
-                                .build()
-                )
-                .filter(DashBoardLineMngResponse::getUseYn)
-                .flatMap(c ->
-                        Mono.just(c)
-                                .zipWith(projectInfoDomainService.findByCustomBusinessNetwork(c.getId()).count())
-                                .map(t -> {
-                                    t.getT1().setCount(t.getT2());
-                                    return t.getT1();
-                                })
-                )
-                .collectList();
-    }
+  /**
+   * 계열관리 통계 모두 가져오기.
+   *
+   * @return DashBoardLineMngResponse line mng
+   */
+  public Mono<List<DashBoardLineMngResponse>> getLineMng() {
+    return lineMngDomainService.findAll()
+        .map(lineMng ->
+            DashBoardLineMngResponse.builder()
+                .id(lineMng.getId())
+                .name(lineMng.getName())
+                .type(lineMng.getType())
+                .useYn(lineMng.getUseYn())
+                .build()
+        )
+        .filter(DashBoardLineMngResponse::getUseYn)
+        .flatMap(c ->
+            Mono.just(c)
+                .zipWith(projectInfoDomainService.findByCustomBusinessNetwork(c.getId()).count())
+                .map(t -> {
+                  t.getT1().setCount(t.getT2());
+                  return t.getT1();
+                })
+        )
+        .collectList();
+  }
 }
